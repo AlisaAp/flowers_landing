96cfadf590ee360ae892714e0d077cedce22b99f
Alisa Apotovska <masanaapot@gmail.com>
2023-02-27 12:38:28 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('burger').onclick = function () {
+  document.getElementById('nav').classList.toggle('active');
+  document.getElementById('burger').classList.toggle('active');
+};
